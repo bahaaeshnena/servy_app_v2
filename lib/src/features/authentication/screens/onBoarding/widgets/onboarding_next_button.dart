be54f727e2ab38510fc7a0891b59210e7820953b
bahaa <eshnenabahaa@gmail.com>
2024-03-20 05:39:43 +0300

    edit theme mode

--- lib/src/features/authentication/screens/onBoarding/widgets/onboarding_next_button.dart
+++ lib/src/features/authentication/screens/onBoarding/widgets/onboarding_next_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:servy_app/src/features/authentication/controllers/onboarding/onboarding_controller.dart';
-import 'package:servy_app/src/utils/constants/colors.dart';
 import 'package:servy_app/src/utils/constants/sizes.dart';
 import 'package:servy_app/src/utils/device/device_utility.dart';
 import 'package:servy_app/src/utils/helpers/helper_function.dart';
@@ -13,15 +12,16 @@ class OnBoardingNextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
+    final darkMode = THelperFunctions.isDarkMode(context);
+    final ThemeData theme = Theme.of(context);
+    final Color buttonColor = darkMode ? theme.primaryColor : Colors.black;
     return Positioned(
       right: TSizes.defaultSpace,
       bottom: TDeviceUtils.getBottomNavigationBarHeight(),
       child: ElevatedButton(
         onPressed: () => OnBoardingController.instance.nextPage(),
         style: ElevatedButton.styleFrom(
-            shape: const CircleBorder(),
-            backgroundColor: dark ? TColors.primaryColor : Colors.red),
+            shape: const CircleBorder(), backgroundColor: buttonColor),
         child: const Icon(Iconsax.arrow_right_3),
       ),
     );
