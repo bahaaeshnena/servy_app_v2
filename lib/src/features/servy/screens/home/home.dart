23a59ed4102d8eee3f735afd2f82b3f54788997b
bahaa <eshnenabahaa@gmail.com>
2024-03-23 01:25:09 +0300

    design ListView categoris and Search BAr

--- lib/src/features/servy/screens/home/home.dart
+++ lib/src/features/servy/screens/home/home.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:servy_app/src/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:servy_app/src/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:servy_app/src/common/widgets/texts/section_heading.dart';
 import 'package:servy_app/src/features/servy/screens/home/widgets/home_appbar.dart';
+import 'package:servy_app/src/features/servy/screens/home/widgets/home_categories.dart';
+
+import 'package:servy_app/src/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,6 +21,21 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 children: [
                   THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+                  TSearchContainer(text: "Search in Servy"),
+                  SizedBox(height: TSizes.spaceBtwSections),
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        TSectionHeading(
+                            title: "Popular Categories",
+                            showActionButton: false),
+                        SizedBox(height: TSizes.spaceBtwItems),
+                        THomeCategories()
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
