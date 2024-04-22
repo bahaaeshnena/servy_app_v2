25c2d05ef91b3b4e9b3a2d13e8b933764d16f985
bahaa <eshnenabahaa@gmail.com>
2024-04-22 18:38:20 +0300

    edit image settings

--- lib/src/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/src/common/widgets/list_tiles/user_profile_tile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:servy_app/src/common/widgets/images/circular_image.dart';
 import 'package:servy_app/src/features/personalization/controllers/user_controller.dart';
 import 'package:servy_app/src/utils/constants/colors.dart';
 import 'package:servy_app/src/utils/constants/images.dart';
+import 'package:servy_app/src/utils/shimmer/shimmer_effect.dart';
 
 class TUserProfileTile extends StatelessWidget {
   const TUserProfileTile({
@@ -15,8 +17,23 @@ class TUserProfileTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = UserController.instance;
     return ListTile(
-      leading: const TCircularImage(
-          image: TImages.user, width: 50, height: 50, padding: 0),
+      leading: Obx(() {
+        final networkImage = controller.user.value.profilePicture;
+        final image = networkImage.isNotEmpty ? networkImage : TImages.user;
+
+        return controller.imageUploading.value
+            ? const TShimmerEffect(
+                width: 50,
+                height: 50,
+                radius: 80,
+              )
+            : TCircularImage(
+                image: image,
+                width: 50,
+                height: 50,
+                isNetworkImage: networkImage.isNotEmpty,
+              );
+      }),
       title: Text(
         controller.user.value.fullName,
         style: Theme.of(context)
