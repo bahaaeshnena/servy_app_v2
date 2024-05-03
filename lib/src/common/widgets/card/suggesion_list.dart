3d799b45a4041843f140a8a9b1bdf11c0a21751e
bahaa <eshnenabahaa@gmail.com>
2024-05-03 04:33:28 +0300

    display services in home page

--- lib/src/common/widgets/card/suggesion_list.dart
+++ lib/src/common/widgets/card/suggesion_list.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:servy_app/src/common/widgets/card/service_card_abstract.dart';
+import 'package:servy_app/src/features/servy/models/service_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SuggestionList extends StatelessWidget {
-  const SuggestionList({super.key});
+  const SuggestionList({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -19,19 +24,32 @@ class SuggestionList extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 12),
-          // SizedBox(
-          //   height: 360,
-          //   width: double.infinity,
-          //   child: ListView.builder(
-          //     scrollDirection: Axis.vertical,
-          //     itemCount: 7,
-          //     itemBuilder: (context, index) => const HomeCard(
-          //       title: '',
-          //       desc: '',
-          //       price: '',
-          //     ),
-          //   ),
-          // )
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('Posts').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                // Filter the list of services based on status
+                List<ServiceModel> services = snapshot.data!.docs
+                    .map((doc) => ServiceModel.fromSnapshot(doc))
+                    .where((service) => service.status == 'accepted')
+                    .toList();
+                return Column(
+                  children: services.map((service) {
+                    return ServiceCardAbstract(
+                      title: service.title,
+                      desc: service.descreption,
+                      price: service.priceFrom,
+                      priceFromDescount: service.priceFromDescount,
+                    );
+                  }).toList(),
+                );
+              }
+            },
+          ),
         ],
       ),
     );
