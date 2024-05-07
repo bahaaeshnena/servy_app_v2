ebe884d3a3322596c84abf4f94c88f28dc6d79b7
bahaa <eshnenabahaa@gmail.com>
2024-05-08 00:00:07 +0300

    finish search logic

--- lib/src/common/widgets/custom_shapes/containers/search_delegate.dart
+++ lib/src/common/widgets/custom_shapes/containers/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:servy_app/src/features/servy/models/service_model.dart';
+import 'package:servy_app/src/common/widgets/card/service_card_abstract.dart';
 
 class CustomSearchDelegate extends SearchDelegate<String> {
   final Function(String) onSearchTextChanged;
@@ -29,18 +32,80 @@ class CustomSearchDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container(); // Implement your search results UI here
+    return Container(); // We'll display results in SuggestionList
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // You can build suggestions here based on query
-    return Container(); // Implement your search suggestions UI here
+    return FutureBuilder<List<ServiceModel>>(
+      future: _searchServices(query),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else {
+          final List<ServiceModel> services = snapshot.data ?? [];
+          if (services.isEmpty) {
+            return const Center(
+              child: Text('No results found'),
+            );
+          } else {
+            return SuggestionList(services: services);
+          }
+        }
+      },
+    );
+  }
+
+  Future<List<ServiceModel>> _searchServices(String query) async {
+    try {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await FirebaseFirestore.instance
+              .collection('Services')
+              .where('title', isGreaterThanOrEqualTo: query)
+              .where('title', isLessThan: '${query}z')
+              .get();
+      return snapshot.docs
+          .map((doc) => ServiceModel.fromSnapshot(doc))
+          .toList();
+    } catch (e) {
+      throw Exception('Error searching services: $e');
+    }
   }
 
   @override
   void showResults(BuildContext context) {
     super.showResults(context);
     onSearchTextChanged(query);
+  }
+}
+
+class SuggestionList extends StatelessWidget {
+  final List<ServiceModel> services;
+
+  const SuggestionList({super.key, required this.services});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: services.map((service) {
+          return ServiceCardAbstract(
+            title: service.title,
+            desc: service.descreption,
+            price: service.priceFrom,
+            priceFromDescount: service.priceFromDescount,
+            imageUrl: service.imageService,
+            isLoadingImage: false,
+            serviceId: '', service: service, // عرض الصورة المحملة
+          );
+        }).toList(),
+      ),
+    );
   }
 }
